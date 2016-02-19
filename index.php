ac5b580e137ba468fdd208e2ae10328513bb0814
Ivo Petkov <ivo@ivopetkov.com>
2016-02-19 16:54:40 +0200

    New API update

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  */
 
 $app->hooks->add('responseCreated', function($response) use ($app, $context) {
-    $options = $context->getOptions();
+    $options = $context->options;
     if (!empty($options['trackingID'])) {
         $code = '<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');ga(\'create\',\'' . htmlentities($options['trackingID']) . '\',\'auto\');ga(\'send\',\'pageview\');</script>';
         $response->content = $app->components->insertHTML($response->content, $code, 'beforeBodyEnd');
