ca6794a581777f2a83b5bf98dd6be54523254a67
Ivo Petkov <ivo@ivopetkov.com>
2016-02-19 16:49:53 +0200

    Name change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  * Google Analytics addon for Bear Framework
- * https://github.com/ivopetkov/bearframework-google-analytics
+ * https://github.com/ivopetkov/google-analytics-bearframework-addon
  * Copyright (c) 2016 Ivo Petkov
  * Free to use under the MIT license.
  */
