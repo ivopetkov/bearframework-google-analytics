8b1868e4ab1e890ff09f1feb0fcd8e32ae3e1712
Ivo Petkov <ivo@ivopetkov.com>
2016-02-20 16:18:42 +0200

    Added addon registration autoloader.

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Google Analytics addon for Bear Framework
+ * https://github.com/ivopetkov/google-analytics-bearframework-addon
+ * Copyright (c) 2016 Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
+BearFramework\Addons::register('ivopetkov/google-analytics-bearframework-addon', __DIR__);
