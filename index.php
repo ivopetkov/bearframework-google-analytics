6daed793611fe94575c635c221b0ebd055f52519
Ivo Petkov <ivo@ivopetkov.com>
2016-04-13 13:15:40 +0300

    The library ivopetkov/html5-dom-document-php is now used to insert the script tag into the page.

--- index.php
+++ index.php
@@ -7,10 +7,18 @@
  * Free to use under the MIT license.
  */
 
-$app->hooks->add('responseCreated', function($response) use ($app, $context) {
-    $options = $context->options;
-    if (!empty($options['trackingID'])) {
-        $code = '<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');ga(\'create\',\'' . htmlentities($options['trackingID']) . '\',\'auto\');ga(\'send\',\'pageview\');</script>';
-        $response->content = $app->components->insertHTML($response->content, $code, 'beforeBodyEnd');
+use BearFramework\App;
+use IvoPetkov\HTML5DOMDocument;
+
+$app->hooks->add('responseCreated', function($response) use ($context) {
+    if ($response instanceof App\Response\HTML) {
+        $options = $context->options;
+        if (!empty($options['trackingID'])) {
+            $domDocument = new HTML5DOMDocument();
+            $domDocument->loadHTML($response->content);
+            $code = '<script>(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');ga(\'create\',\'' . htmlentities($options['trackingID']) . '\',\'auto\');ga(\'send\',\'pageview\');</script>';
+            $domDocument->insertHTML($code);
+            $response->content = $domDocument->saveHTML();
+        }
     }
 });
